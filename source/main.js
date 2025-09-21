3aaf24bcb90ea73356d19de00886df33c0cd615b
Alex Pooley (@zuedev) <zuedev@gmail.com>
2025-09-21 16:58:54 +0100

    update dependencies and refactor instrumentation

--- source/main.js
+++ source/main.js
@@ -1,10 +1,7 @@
-// sentry error tracking
-import "./instrument.js";
-
 import bot from "./bot.js";
 
 // check required environment variables are set
-["ENVIRONMENT", "DISCORD_BOT_TOKEN", "MONGODB_URI"].forEach((env) => {
+["DISCORD_BOT_TOKEN", "MONGODB_URI"].forEach((env) => {
   if (!process.env[env])
     throw new Error(`Environment variable ${env} is required!`);
 });
