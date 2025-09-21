f3df71cf25d92bb2aa8046784a27ef2239806f5e
Alex Pooley (@zuedev) <zuedev@gmail.com>
2025-09-21 23:04:03 +0100

    add docs comment to roll

--- source/library/roll.js
+++ source/library/roll.js
@@ -1,3 +1,11 @@
+/**
+ * Rolls a specified number of dice with a given number of sides and returns the results and total.
+ *
+ * @param {string} dice - The dice notation string (e.g., "2d6" for rolling two six-sided dice).
+ * @returns {{ results: number[], total: number }} An object containing an array of individual roll results and their total sum.
+ * @example
+ * roll("3d4"); // { results: [2, 4, 1], total: 7 }
+ */
 export default function roll(dice) {
   const [quantity, sides] = dice.split("d").map((x) => parseInt(x));
 
