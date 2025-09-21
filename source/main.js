99ff40639c0835cd5345db3c9af5e0c66b0dd573
Alex Pooley (@zuedev) <zuedev@gmail.com>
2025-09-21 17:13:46 +0100

    does our mongodb uri contain a db name?

--- source/main.js
+++ source/main.js
@@ -6,5 +6,11 @@ import bot from "./bot.js";
     throw new Error(`Environment variable ${env} is required!`);
 });
 
+// does our mongodb uri contain a db name?
+if (!process.env.MONGODB_URI.match(/\/([a-zA-Z0-9-_]+)(\?|$)/))
+  throw new Error(
+    "MONGODB_URI must contain a database name! Example: mongodb+srv://user:password@cluster.mongodb.net/mydatabase"
+  );
+
 // start the bot
 await bot();
